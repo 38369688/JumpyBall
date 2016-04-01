c845f5ed009a8494643a70640dadd09e691c8631
38369688 <alextsai@Alexs-MacBook-Pro-3.local>
2016-04-01 08:29:23 -0700

    initial checkin

---
+++ src/jumpyball/JumpyBall.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jumpyball;
+
+import environment.ApplicationStarter;
+
+
+/**
+ *
+ * @author alextsai
+ */
+public class JumpyBall {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ApplicationStarter.run("Goooo!!!", new space());
+               
+    }
+    
+}
